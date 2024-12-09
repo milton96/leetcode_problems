2671c46a80866ae9b44106168a073913f72729a2
Milton Bravo <miltonbravo96@gmail.com>
2024-12-08 21:40:02 -0600

    ajustes a solucion

--- 121_best_time_to_buy_and_sell_stock.py
+++ 121_best_time_to_buy_and_sell_stock.py
@@ -1,15 +1,30 @@
 
 def maxProfit(prices: list[int]) -> int:
-    m = min(prices)
-    if m == prices[-1]:
+    min_value = prices[0]
+    for i in range(1, len(prices)):
+        if prices[i] <= min_value:
+            min_value = prices[i]
+        else:
+            m = min(prices[i+1:])
+            if m <= min_value:
+                min_value = m
+            break
+    index = prices.index(min_value)
+    prices_sliced = prices[index+1:]
+    if len(prices_sliced) == 0:
         return 0
-    i = prices.index(m)
-    m_v = max(prices[i+1:])
-    return m_v - m
-
+    max_value = max(prices[index+1:])
+    return max_value - min_value
 
 
 if __name__ == "__main__":
-    prices = [7,1,5,3,6,4]
-    m = maxProfit(prices)
-    print("hola")
+    prices = [
+        [7,1,5,3,6,4],
+        [7,6,4,3,1],
+        [2,4,1],
+        [2,1,2,0,1],
+        [2,1,2,1,0,1,2]
+    ]
+    for price in prices:
+        sol = maxProfit(price)
+        print(sol)
